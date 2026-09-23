d2f4ad494e8ef3a37b0617a8e9ad0384ce135167
Brendigo <info@brendigo.com>
2026-09-24 00:02:51 +0200

    feat: load WooCommerce Sidrena CSV integration

--- sidrena.php
+++ sidrena.php
@@ -38,6 +38,7 @@ require_once SIDRENA_DIR . 'includes/class-sidrena-service-history.php';
 require_once SIDRENA_DIR . 'includes/class-sidrena-location-data.php';
 require_once SIDRENA_DIR . 'includes/class-sidrena-location-history.php';
 require_once SIDRENA_DIR . 'includes/class-sidrena-products.php';
+require_once SIDRENA_DIR . 'includes/class-sidrena-woo-import-export.php';
 require_once SIDRENA_DIR . 'includes/class-sidrena-services.php';
 require_once SIDRENA_DIR . 'includes/class-sidrena-pricelist.php';
 require_once SIDRENA_DIR . 'includes/class-sidrena-rest.php';
