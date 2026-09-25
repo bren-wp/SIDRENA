9a50284e6accd8812530b46aa9cc7a537cc0793a
Brendigo <info@brendigo.com>
2026-09-25 10:04:06 +0200

    Load simplified admin menu UX layer

--- includes/sidrena-bootstrap.php
+++ includes/sidrena-bootstrap.php
@@ -42,6 +42,7 @@ if ( 'wordpress' === SIDRENA_EDITION ) {
 }
 
 require_once SIDRENA_DIR . 'includes/class-sidrena-admin.php';
+require_once SIDRENA_DIR . 'includes/class-sidrena-admin-ux.php';
 require_once SIDRENA_DIR . 'includes/class-sidrena-plugin.php';
 
 if ( ! function_exists( 'sidrena_cijena' ) ) {
