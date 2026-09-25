f9e12103f2c8f017f119e19d8a28d4e836e81450
Brendigo <info@brendigo.com>
2026-09-25 11:57:52 +0200

    Prepare sidrena-wordpress 0.8.1 WordPress.org metadata

--- editions/wordpress/sidrena-wordpress.php
+++ editions/wordpress/sidrena-wordpress.php
@@ -1,17 +1,16 @@
 <?php
 /**
  * Plugin Name: Sidrena WordPress
- * Plugin URI: https://sidrene-cijene.com.hr/
+ * Plugin URI: https://brendigo.com/sidrene-cijene/
  * Description: Sidrene cijene za običan WordPress bez WooCommercea: vlastiti katalog proizvoda i usluga, CSV/XML cjenici i arhiva 30+ dana.
- * Version: 0.7.0
+ * Version: 0.8.1
  * Requires at least: 6.6
  * Requires PHP: 7.4
  * Author: Brendigo
  * Author URI: https://brendigo.com/
- * License: Sidrena Software License 1.0
- * License URI: https://github.com/bren-wp/SIDRENA/blob/main/LICENSE
- * Update URI: https://github.com/bren-wp/SIDRENA#sidrena-wordpress
- * Text Domain: sidrena
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: sidrena-wordpress
  * Domain Path: /languages
  */
 
@@ -61,7 +60,7 @@ if ( defined( 'SIDRENA_EDITION' ) || class_exists( 'Sidrena_Plugin', false ) || 
 	return;
 }
 
-define( 'SIDRENA_VERSION', '0.7.0' );
+define( 'SIDRENA_VERSION', '0.8.1' );
 define( 'SIDRENA_EDITION', 'wordpress' );
 define( 'SIDRENA_RULESET', 'NN 101/2026 · NN 105/2026 · MINGO 22.09.2026' );
 define( 'SIDRENA_RULES_EFFECTIVE', '2026-10-01' );
